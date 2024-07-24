237379b72f4cfc5489edc98824d0d9d473636dcf
ShockWaveGT <nicooavenged@gmail.com>
2024-07-24 11:18:19 -0300

    Initialize Project
    Create the Springboot project

---
+++ src/main/java/com/example/petscreening/PetScreeningApplication.java
@@ -0,0 +1,16 @@
+package com.example.petscreening;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PetScreeningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetScreeningApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/example/petscreening/PetScreeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petscreening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetScreeningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
