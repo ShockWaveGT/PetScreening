a1f7e33407fd2b95d712acedf4aac1ce4a3bad6d
ShockWaveGT <nicooavenged@gmail.com>
2024-07-25 11:50:11 -0300

    Add functionality to breed and pet repositories
    Add all functions to repositories to handle the queries

--- src/main/java/com/example/petscreening/pet/repository/BreedRepository.java
+++ src/main/java/com/example/petscreening/pet/repository/BreedRepository.java
@@ -2,6 +2,8 @@ package com.example.petscreening.pet.repository;
 
 import com.example.petscreening.pet.entity.Breed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BreedRepository extends JpaRepository<Breed, Long> {
 }
--- src/main/java/com/example/petscreening/pet/repository/PetRepository.java
+++ src/main/java/com/example/petscreening/pet/repository/PetRepository.java
@@ -1,7 +1,24 @@
 package com.example.petscreening.pet.repository;
 
+import com.example.petscreening.pet.entity.Breed;
 import com.example.petscreening.pet.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
+    
+    public List<Pet> findAllByWeightLessThan(double weight);
+
+    public List<Pet> findByVaccinationStatus(@Param ("isVaccinated") boolean vaccinationStatus);
+    
+    @Query("SELECT p FROM Pet p WHERE p.breedID.name <> :breedName")
+    public List<Pet> findByBreedNameNot(@Param("breedName") String breedName);
+
+    public List<Pet> findAllByTrainingLevelGreaterThan(double trainingLevel);
+
 }
